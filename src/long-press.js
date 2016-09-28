695ad0ac6bef9779d8b9549b64333499c400b39b
Vladimir Ruzic <vruzic@ztech.io>
2016-09-28 16:27:32 +0200

    creating lib architecture

--- src/long-press.js
+++ src/long-press.js
@@ -3,18 +3,49 @@
     function  isString(element) {
         return (typeof element === 'string' || element instanceof String)
     }
+    function isDomElement(element){
+        return (
+            typeof HTMLElement === "object" ? element instanceof HTMLElement : //DOM2
+            element && typeof element === "object" && element !== null && element.nodeType === 1 && typeof element.nodeName==="string"
+        );
+    }
 
-    function LongPress(element) {
+    function bind(element) {
+        this.testNumber = 20;
         if(!element) {
-            /* TODO default constructor
-            *  Should search for all elements with data-on-long-press attribute and try to eval
-            * */
+            /* TODO
+             *  Should search for all elements with data-on-long-press attribute and try to eval
+             * */
+            console.log('search')
         } else if (isString(element)) {
-
+            console.log('string')
+        } else if (isDomElement(element)) {
+            console.log('dom element')
+        } else {
+            var error = {
+                "message": 'Invalid arguments',
+                "expected": ['DOM element', '[DOM element]', 'String', 'undefined/null'],
+                "got": {
+                    "value": element,
+                    "type": typeof element
+                }
+            };
+            console.error(error);
+            throw error;
         }
     }
-    if(!window.LongPress) {
-        window.LongPress = LongPress;
+    function unbind() {
+
+    }
+    function LongPress() {
+        this.boundElements = [];
+        this.bind = bind;
+        this.unbind = unbind;
+    }
+
+
+    if(!window.longPress) {
+        window.longPress =  new LongPress;
     }
 
 }());
