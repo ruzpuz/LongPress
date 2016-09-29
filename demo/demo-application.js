ef60b4a4cb147140945fd3bd382228fda041e032
Vladimir Ružić <vladimir@ruzic.info>
2016-09-29 17:55:57 +0200

    selecting elements

--- demo/demo-application.js
+++ demo/demo-application.js
@@ -1,6 +1,9 @@
 (function () {
     'use strict';
 
+    longPress.setLongClickDuration(1500);
+    try{longPress.bind()} catch (e) {
+        console.log(e)
+    }
     console.log(longPress);
-
 }());
--- src/long-press.js
+++ src/long-press.js
@@ -1,6 +1,13 @@
 (function () {
+    'use strict';
 
-    function  isString(element) {
+    var defaultLongClickDuration = 500;
+
+    function isInt(value) {
+        var x;
+        return isNaN(value) ? !1 : (x = parseFloat(value), (0 | x) === x);
+    }
+    function isString(element) {
         return (typeof element === 'string' || element instanceof String)
     }
     function isDomElement(element){
@@ -9,14 +16,13 @@
             element && typeof element === "object" && element !== null && element.nodeType === 1 && typeof element.nodeName==="string"
         );
     }
-
-    function bind(element) {
-        this.testNumber = 20;
+    function getDomElements(element) {
         if(!element) {
             /* TODO
              *  Should search for all elements with data-on-long-press attribute and try to eval
              * */
             console.log('search')
+            return document.querySelectorAll('[data-on-long-press]')
         } else if (isString(element)) {
             console.log('string')
         } else if (isDomElement(element)) {
@@ -30,20 +36,45 @@
                     "type": typeof element
                 }
             };
-            console.error(error);
-            throw error;
+            console.log(error);
+            throw new Error(error);
         }
+    }
+
+    function bind(element) {
+        var elements = getDomElements(element);
+        console.log(elements)
     }
     function unbind() {
 
     }
+    function setLongClickDuration(duration) {
+        if(!isInt(duration)) {
+            var error = {
+                "message": 'Invalid argument',
+                "expected": 'Integer',
+                "got": {
+                    "value": duration,
+                    "type": typeof duration
+                }
+            };
+            console.log(error);
+            throw error;
+        } else {
+            this.longClickDuration = duration;
+        }
+    }
     function LongPress() {
+
         this.boundElements = [];
+        this.longClickDuration = defaultLongClickDuration;
+
         this.bind = bind;
         this.unbind = unbind;
+        this.setLongClickDuration = setLongClickDuration;
+
     }
-
-
+    
     if(!window.longPress) {
         window.longPress =  new LongPress;
     }
