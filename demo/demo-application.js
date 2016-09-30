ae601eda646f3ffc3d4bb6eb7d6be083fbdabc92
Vladimir Ruzic <vruzic@ztech.io>
2016-09-30 13:50:57 +0200

    selecting dom elements

--- demo/demo-application.js
+++ demo/demo-application.js
@@ -2,7 +2,8 @@
     'use strict';
 
     longPress.setLongClickDuration(1500);
-    try{longPress.bind()} catch (e) {
+
+    try{longPress.bind(document.querySelectorAll('[data-on-long-press]'))} catch (e) {
         console.log(e)
     }
     console.log(longPress);
--- src/long-press.js
+++ src/long-press.js
@@ -16,18 +16,21 @@
             element && typeof element === "object" && element !== null && element.nodeType === 1 && typeof element.nodeName==="string"
         );
     }
+    function isNodeList(element) {
+        console.log(typeof element.length === 'number'
+            && typeof element.item !== 'undefined'
+            && typeof element.nextNode === 'function'
+            && typeof element.reset === 'function');
+
+        return (typeof element.length == 'number'
+        && typeof element.item == 'function'
+        && typeof element.nextNode == 'function'
+        && typeof element.reset == 'function');
+    }
+
     function getDomElements(element) {
-        if(!element) {
-            /* TODO
-             *  Should search for all elements with data-on-long-press attribute and try to eval
-             * */
-            console.log('search')
-            return document.querySelectorAll('[data-on-long-press]')
-        } else if (isString(element)) {
-            console.log('string')
-        } else if (isDomElement(element)) {
-            console.log('dom element')
-        } else {
+
+        function throwError(details) {
             var error = {
                 "message": 'Invalid arguments',
                 "expected": ['DOM element', '[DOM element]', 'String', 'undefined/null'],
@@ -36,14 +39,47 @@
                     "type": typeof element
                 }
             };
+            details ? error.got.details = details : {};
             console.log(error);
             throw new Error(error);
         }
+
+        console.log(Array.prototype.slice.call(element))
+        if(!element) {
+            /* TODO
+             *  Should search for all elements with data-on-long-press attribute and try to eval
+             * */
+            Array.prototype.slice.call(document.querySelectorAll('[data-on-long-press]'));
+        } else if (isString(element)) {
+            return document.querySelectorAll(element);
+        } else if(isNodeList(element)) {
+            return Array.prototype.slice.call(element);
+        } else if(Array.isArray(element)) {
+
+            var length = element.length,
+                i;
+
+            for(i = 0; i < length; i += 1) {
+                if(!isDomElement(element[i])) {
+                    throwError({
+                        "message" : 'At least one element in the array is not DOM element.',
+                        "firstInvalidElement" : element[i]
+                    });
+                }
+            }
+            return Array.prototype.slice.call(element);
+
+        } else if (isDomElement(element)) {
+            return [element];
+        } else {
+            throwError();
+        }
+
     }
 
     function bind(element) {
         var elements = getDomElements(element);
-        console.log(elements)
+        console.log(elements[0])
     }
     function unbind() {
 
