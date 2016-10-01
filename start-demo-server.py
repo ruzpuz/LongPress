4924424472297d64994062aedb8ceec92bce6956
Marko Mudrinic <xmudrii@users.noreply.github.com>
2016-10-01 18:50:09 +0200

    Fix server to correct serve js file

--- start-demo-server.py
+++ start-demo-server.py
@@ -14,6 +14,7 @@ class demoAppHandler(BaseHTTPRequestHandler):
 			mimetype='text/html'
 			sendReply = True
 		if self.path.endswith(".js"):
+			self.path="/demo/demo-application.js"
 			mimetype='application/javascript'
 			sendReply = True
 		
