6f48ddfcd91b531e3c0620778421fe819cf3455a
Marko Mudrinic <xmudrii@users.noreply.github.com>
2016-10-01 18:37:48 +0200

    Changed server to serve content instead of dir

--- start-demo-server.py
+++ start-demo-server.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python
 
-import SimpleHTTPServer
-import SocketServer
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from os import curdir, sep
 
 PORT = 8000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-Handler.extensions_map.update({
-    '.webapp': 'application/x-web-app-manifest+json',
-});
+class demoAppHandler(BaseHTTPRequestHandler):
+	
+	def do_GET(self):
+		if self.path=="/": self.path="/demo/index.html"
+		sendReply = False
+		if self.path.endswith(".html"):
+			mimetype='text/html'
+			sendReply = True
+		if self.path.endswith(".js"):
+			mimetype='application/javascript'
+			sendReply = True
+		
+		if sendReply == True:
+			f = open(curdir + sep + self.path) 
+			self.send_response(200)
+			self.send_header('Content-type',mimetype)
+			self.end_headers()
+			self.wfile.write(f.read())
+			f.close()
+		return
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print "Serving at port", PORT
+httpd = HTTPServer(('', PORT), demoAppHandler)
+
+print 'Serving at port:', PORT
 httpd.serve_forever()
