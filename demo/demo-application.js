6d51030990f286f59682f87c30e855d238729eac
Vladimir Ružić <vladimir@ruzic.info>
2016-10-14 20:20:14 +0200

    unbinding elements

--- demo/demo-application.js
+++ demo/demo-application.js
@@ -2,10 +2,13 @@
     'use strict';
     longPress.bind(document.querySelector('#second'), null, 500);
 
-    try{
+   /* try{
         //longPress.bind(document.querySelector('#second'));
         console.log(longPress)
     } catch (e) {
         console.log(e)
-    }
+    }*/
+    longPress.unbind(document.querySelector('#second'));
+    console.log(longPress);
+
 }());
